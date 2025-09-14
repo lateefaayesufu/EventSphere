9353d896ef88dfeb796ee619741acb9d133c3e31
divineamunega <divineamunega@gmail.com>
2025-09-14 12:19:57 +0100

    Refactor Button component to allow custom onClick handling and props forwarding

--- frontend/src/components/sections/ui/Button.jsx
+++ frontend/src/components/sections/ui/Button.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import withAuth from "./withAuth";
-import { useNavigate } from "react-router";
 
-const Button = ({ children, onClick, requireAuth = false }) => {
-	const navigate = useNavigate();
-	const ButtonComponent = (
-		<button
-			type="button"
-			onClick={() => navigate("/login")}
-			className="bg-white text-black w-fit px-5 py-3 font-semibold rounded-md hover:scale-101 cursor-pointer active:scale-95 transition-transform"
-		>
-			{children}
-		</button>
-	);
+const Button = ({ children, onClick, requireAuth = false, ...props }) => {
+  const ButtonComponent = (
+    <button
+      type="button"
+      onClick={onClick}
+      className="bg-white text-black w-fit px-5 py-3 font-semibold rounded-md hover:scale-101 cursor-pointer active:scale-95 transition-transform"
+      {...props}
+    >
+      {children}
+    </button>
+  );
 
-	const AuthenticatedButton = withAuth(() => ButtonComponent);
+  const AuthenticatedButton = withAuth((authProps) => (
+    <button
+      type="button"
+      {...props}
+      {...authProps}
+      className="bg-white text-black w-fit px-5 py-3 font-semibold rounded-md hover:scale-101 cursor-pointer active:scale-95 transition-transform"
+    >
+      {children}
+    </button>
+  ));
 
-	return requireAuth ? <AuthenticatedButton /> : ButtonComponent;
+  return requireAuth ? <AuthenticatedButton onClick={onClick} /> : ButtonComponent;
 };
 
 export default Button;
